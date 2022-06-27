5a6e91b356acc032dcd4bcf9bfffa7f3257f2e0e
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-06-27 15:28:08 -0300

    disabled aula em breve

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -26,7 +26,10 @@ export function Lesson(props: LessonProps) {
   const isActiveLesson = slug === props.slug;
 
   return (
-    <Link to={`/event/lesson/${props.slug}`} className="group">
+    <Link
+      to={isLessonAvailable ? `/event/lesson/${props.slug}` : "#"}
+      className="group"
+    >
       <span className="text-gray-300">{availableDateFormatted}</span>
       <div
         className={classNames(
